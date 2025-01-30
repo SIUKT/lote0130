96c9daa814283f4b219c71b761fcadf15018874d
SIUKT <894695703@qq.com>
2025-01-30 22:08:58 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/lote0130/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lote0130
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'lote',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff8acf00)),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
